9d45046b0ae8d41c87a39e5c4cd05d06fb917355
huangkangjia <951530315@qq.com>
2019-09-25 16:56:15 +0800

    添加了h2 h3

--- src/components/01.index.vue
+++ src/components/01.index.vue
@@ -3,7 +3,8 @@
         <div class="section">
             <div class="location">
                 <h1>hello world</h1>
-                <p>这是分支合并才能的！</p>
+                <h2>使用分支</h2>
+                <h3>使用分支</h3>
                 <span>当前位置：</span>
                 <router-link to="/" class="router-link-active">首页</router-link> &gt;
                 <router-link to="/" class="router-link-exact-active router-link-active">购物商城</router-link>
